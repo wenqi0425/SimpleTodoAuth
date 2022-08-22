3b0975eff4dd78f9153b5910be1f6a9b25995689
wenqi0425 <xiaoc288@gmail.com>
2022-08-22 09:27:08 +0200

    ToDoTaskModel

---
+++ Models/ToDoTaskModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleTodoAuth.Models
+{
+    public class ToDoTaskModel
+    {
+        [Key]
+        public int TaskId { get; set; }
+
+        [Required(ErrorMessage = "TaskName is required")]
+        public string TaskName { get; set; }
+
+        public string TaskDescription { get; set; }
+        public bool TaskStatus { get; set; }
+    }
+}
