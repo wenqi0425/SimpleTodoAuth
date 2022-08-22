13f5e3cf8d791edfd830ed45b99b06db19240fb5
wenqi0425 <xiaoc288@gmail.com>
2022-08-22 09:41:24 +0200

    TodoTaskModel TypeName ApplicationDbContext

--- Models/ToDoTaskModel.cs
+++ Models/ToDoTaskModel.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
 namespace SimpleTodoAuth.Models
 {
-    public class ToDoTaskModel
+    public class TodoTaskModel
     {
         [Key]
         public int TaskId { get; set; }
 
         [Required(ErrorMessage = "TaskName is required")]
+        [Column(TypeName = "nvarchar(100)")]
         public string TaskName { get; set; }
 
+        [Column(TypeName = "nvarchar(200)")]
         public string TaskDescription { get; set; }
+
+        [Column(TypeName = "bit")]
         public bool TaskStatus { get; set; }
     }
 }
---
+++ SimpleTodoDbContext/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using SimpleTodoAuth.Authentication;
+using SimpleTodoAuth.Models;
+
+namespace SimpleTodoAuth.SimpleTodoDbContext
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base(options) 
+        { 
+        }
+
+        public DbSet<TodoTaskModel> TodoTasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TodoTaskModel>(entity =>
+            {
+                entity.Property(e => e.TaskName).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.TaskDescription).HasMaxLength(200);
+                entity.Property(e => e.TaskStatus);
+            });
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
