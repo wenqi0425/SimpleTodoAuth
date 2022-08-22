401ccc471e578448de9327990a3517cdb7c09691
wenqi0425 <xiaoc288@gmail.com>
2022-08-22 10:58:45 +0200

    fix a bug in Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -60,9 +60,6 @@ namespace SimpleTodoAuth
                     IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
                 };
             });
-
-            services.AddDbContext<SimpleTodoAuthContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SimpleTodoAuthContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
