4bc7c75d7727ecfbdb870d7e55027ba4309e7850
wenqi0425 <xiaoc288@gmail.com>
2022-08-22 11:50:00 +0200

    modify register

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -33,28 +33,48 @@ namespace SimpleTodoAuth.Controllers
             _configuration = configuration;
         }
 
-        [HttpPost]
         [Route("register")]
         public async Task<IActionResult> Register([FromBody] RegisterModel model)
         {
-            var result = await _userManager.FindByEmailAsync(model.Email);
+            var userExists = await _userManager.FindByNameAsync(model.Username);
 
-            if (result != null)
+            if (userExists != null)
+            {
+                return StatusCode(
+                    StatusCodes.Status500InternalServerError, 
+                    new Response 
+                    { 
+                        Status = "Error", 
+                        Message = "User already exists!" 
+                    });
+            };
+
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (!result.Succeeded)
             {
                 return StatusCode(
                     StatusCodes.Status500InternalServerError,
                     new Response
                     {
                         Status = "Error",
-                        Message = "User already exists!"
+                        Message = "User creation failed! Please check user details and try again."
                     });
-            };
+            }
 
-            return Ok(new Response
-            {
-                Status = "Success",
-                Message = "User created successfully"
-            });
+            return Ok(
+                new Response
+                {
+                    Status = "Success",
+                    Message = "User created successfully"
+                });
         }
 
         [HttpPost]
